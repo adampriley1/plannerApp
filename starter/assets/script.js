9ab68b51e4ca8b2480c4eb336c03b4c8c08c11f4
adampriley1 <adampriley1@hotmail.com>
2023-12-11 11:05:11 +0000

    time background colour changes working

--- starter/assets/script.js
+++ starter/assets/script.js
@@ -1,43 +1,42 @@
-var currentDayDisplayEl = $('#currentDay');
+var currentDayDisplayEl = $("#currentDay");
 //display current date
 
-displayDate(); 
+const scheduleContainer = $('.container');
+
+displayDate();
 
 // handle displaying the time
 function displayDate() {
-
-//eusures current time updated ever second 
-    setInterval(() => { 
-        //gets current time 
-       const now = dayjs(); 
-       //format the time 
-        const formattedTime = now.format('dddd, MMMM D'); 
-        //text field displays formatted time 
-        currentDayDisplayEl.text(formattedTime); 
-    }, 1000); }
-
+  //eusures current time updated ever second
+  setInterval(() => {
+    //gets current time
+    const now = dayjs();
+    //format the time
+    const formattedTime = now.format("dddd, MMMM D");
+    //text field displays formatted time
+    currentDayDisplayEl.text(formattedTime);
+  }, 1000);
+}
 
 //gets the current hour from dayjs
-    const nowHour = dayjs().format('H'); 
+const nowHour = parseInt(dayjs().format("H"));
 
-    //loop to go through each.time-block element
-    
-    $('.time-block').each(function() {
-        $(this) 
-        const elementHour = $(this).attr('data-hour')
+//loop to go through each.time-block element and set class to change colour//
 
-        if (elementHour < nowHour) {
-            $(this).addClass('past');
-        }
-        
+$(".time-block").each(function () {
+  $(this);
+  const elementHour = parseInt($(this).attr("data-hour"));
 
-        if (elementHour < nowHour) {
-            $(this).addClass('present');
-        }
+  if (elementHour < nowHour) {
+    $(this).addClass("past");
+  }
 
+  if (elementHour === nowHour) {
+    $(this).addClass("present");
+  }
 
-        if (elementHour > nowHour) {
-            $(this).attr('class','future')
-        }
-        
-    });
+  if (elementHour > nowHour) {
+    $(this).addClass("future");
+  }
+});
+
